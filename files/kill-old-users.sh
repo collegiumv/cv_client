555f6577b5f5b36e87f44654ea1d4804dee7eaaa
dinoocch <dino.occhialini@gmail.com>
2015-09-24 22:03:07 -0500

    Reduce fibers of old users fiber lamps

--- files/kill-old-users.sh
+++ files/kill-old-users.sh
@@ -9,6 +9,8 @@ if [ "$XSCREENTIME" ]; then
 	XSCREENDATE=$(date -d "$XSCREENTIME" +%s)
 	LOCKEDTIME=$((NOW - XSCREENDATE))
 	if [ "$LOCKEDTIME" -ge "$KILLSECONDS" ]; then
+		sudo -u  $PERSON awk -F " " '$1=="fiberlamp"{if($0 !~ /delay/){sub($2, $2" -delay 0")}if($0 !~ /count/){sub($4,$4" -count 500" )}{sub($6,int($6*.8))}}{print $0}' /home/$PERSON/.xscreensaver | sudo -u $PERSON tee /home/$PERSON/.xscreen
+		sudo -u $PERSON mv /home/$PERSON/.xscreen /home/$PERSON/.xscreensaver
 		pkill -KILL -u $PERSON
 	fi
 fi
