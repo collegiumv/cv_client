eaa7da19e0615c88293fff330561dc986cd22358
Michael Aldridge <aldridge.mac@gmail.com>
2016-07-21 15:00:39 -0500

    Improve screensaver handling

--- files/kill-old-users.sh
+++ files/kill-old-users.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 
+# Seconds before we kill the user.  2700 seconds = 45 minutes
+KILLSECONDS=2700
+
 export DISPLAY=":0.0"
-PERSON=$(who | grep " :0" | cut -d" " -f 1)
-XSCREENTIME=$(sudo -u $PERSON xscreensaver-command --time | grep locked | sed "s/^.*since //")
-KILLSECONDS=2700		#Seconds before we kill the user.  2700 seconds = 45 minutes
-if [ "$XSCREENTIME" ]; then
-	NOW=$(date '+%s')
-	XSCREENDATE=$(date -d "$XSCREENTIME" +%s)
-	LOCKEDTIME=$((NOW - XSCREENDATE))
-	if [ "$LOCKEDTIME" -ge "$KILLSECONDS" ]; then
-		sudo -u  $PERSON awk -F " " '$1=="fiberlamp"{if($0 !~ /delay/){sub($2, $2" -delay 0")}if($0 !~ /count/){sub($4,$4" -count 500" )}{sub($6,int($6*.8))}}{print $0}' /home/$PERSON/.xscreensaver | sudo -u $PERSON tee /home/$PERSON/.xscreen
-		sudo -u $PERSON mv /home/$PERSON/.xscreen /home/$PERSON/.xscreensaver
-		pkill -KILL -u $PERSON
-	fi
+PERSON=$(who | grep ":0" | cut -d" " -f 1)
+LOCKEDTIME=0
+
+if pgrep -u "$PERSON" cinnamon-screen ; then #we check for the shotened name as that's what pgrep returns
+    echo "Using Cinnamon-Screensaver"
+    LOCKEDTIME=$(sudo -u "$PERSON" cinnamon-screensaver-command --time | grep -Eo '[0-9]{1,4}')
+elif pgrep -u "$PERSON" xscreensaver ; then
+    echo "Using XScreenSaver"
+    XSCREENTIME=$(sudo -u "$PERSON" xscreensaver-command --time | grep locked | sed "s/^.*since //; s/(.*$//")
+    if [ -z "$XSCREENTIME" ] ; then
+        echo "XScreenSaver is not currently locked!"
+        exit
+    fi
+    NOW="$(date '+%s')"
+    XSCREENDATE="$(date -d "$XSCREENTIME" +%s)"
+    LOCKEDTIME="$((NOW - XSCREENDATE))"
+    echo "XScreenTime: $XSCREENTIME"
+    echo "Now: $NOW"
+    echo "XScreenDate: $XSCREENDATE"
+fi
+echo "LockedTime: $LOCKEDTIME"
+
+if [ "$LOCKEDTIME" -ge "$KILLSECONDS" ]; then
+    pkill -KILL -u "$PERSON"
 fi
